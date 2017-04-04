2e1fcea4cafaf9fca6ed9eeb8db1395a0afcfa00
Natansab <natansab@gmail.com>
2017-04-04 23:30:25 +0200

    ok;

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,11 +1,20 @@
 <?php
 function ft_is_sort($array1)
 {
+$error = 0;
 $array2 = $array1;
-sort($array1);
-foreach($array1 as $key=>$elem)
-	if ($array2[$key] != $elem)
-		return (0);
-return (1);
+sort($array2);
+foreach($array2 as $key=>$elem)
+	if ($array1[$key] != $elem)
+		$error = 1;
+if (!$error)
+	return 1;
+$array2 = $array1;
+rsort($array2);
+foreach($array2 as $key=>$elem)
+	if ($array1[$key] != $elem)
+		return 0;
+return 1;
 }
+// diff_array_assoc(***)
 ?>
---
+++ d01/ex11/do_op_2.php
@@ -0,0 +1,61 @@
+#!/usr/bin/php
+<?php
+function do_op ($nb1, $op, $nb2)
+{
+if ($op == "%")
+	$result = $nb1 % $nb2;
+else if ($op == "+")
+	$result = $nb1 + $nb2;
+else if ($op == "*")
+	$result = $nb1 * $nb2;
+else if ($op == "-")
+	$result = $nb1 - $nb2;
+else if ($op == "/" && $nb2)
+	$result = $nb1 / $nb2;
+else if ($op == "/" && !$nb2)
+	exit_syntax();
+return $result;
+}
+?>
+<?php
+function exit_syntax()
+{
+	echo "Syntax Error\n";
+	exit ;
+}
+?>
+<?php
+if ($argc != 2)
+{
+	echo "Incorrect Parameters\n";
+	return ;
+}
+$i = 0;
+$argv[1] = trim($argv[1]);
+if ($argv[1][$i] == "-" || $argv[1][$i] == "+")
+	$i++;
+if (!ctype_digit($argv[1][$i]))
+	exit_syntax();
+while (ctype_digit($argv[1][$i]))
+	$i++;
+$nb1 = substr($argv[1], 0, $i);
+while($argv[1][$i] == " ")
+	$i++;
+if (!strstr("+-%/*", $argv[1][$i]))
+	exit_syntax();
+$op = $argv[1][$i];
+$i++;
+while($argv[1][$i] == " ")
+	$i++;
+$j = $i;
+if ($argv[1][$i] == "-" || $argv[1][$i] == "+")
+	$i++;
+if (!ctype_digit($argv[1][$i]))
+	exit_syntax();
+while (ctype_digit($argv[1][$i]))
+	$i++;
+$nb2 = substr($argv[1], $j, $i - $j);
+if ($i != strlen($argv[1]))
+	exit_syntax();
+echo do_op($nb1, $op, $nb2)."\n";
+?>
