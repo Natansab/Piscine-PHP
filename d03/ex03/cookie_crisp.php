9cf162fb8b1ee33529c447104750d0ca7b32aacd
Natansab <natansab@gmail.com>
2017-04-06 16:09:55 +0200

    ok

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,11 @@
+<?php
+if ($_GET["action"] && $_GET["name"])
+{
+	if ($_GET["action"] === "set" && $_GET["value"])
+		setcookie($_GET["name"], $_GET["value"]);
+	else if ($_GET["action"] === "get" && $_COOKIE[$_GET["name"]])
+		echo $_COOKIE[$_GET["name"]]."\n";
+	else if ($_GET["action"] === "del")
+		setcookie($_GET["name"]);
+}
+?>
