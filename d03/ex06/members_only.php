9e6eacd43122ee1a48e159996ad01228e31d68b0
Natansab <natansab@gmail.com>
2017-04-06 19:07:08 +0200

    ok

---
+++ d03/ex06/members_only.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER['PHP_AUTH_USER'] != "zaz" && $_SERVER['PHP_AUTH_PW'] != "jaimelespetitsponeys" ) {
+    header('HTTP/1.0 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm="Espace membres"');
+    echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+    exit;
+} else {
+    echo "<html><body>
+Bonjour Zaz<br /><img src='data:image/png;base64,"
+.base64_encode(file_get_contents("../img/42.png"))."'></body></html>\n";
+}
+?>
