7bd886a4ae3df9a5ec770ad60b41fd3414ae3abf
Natansab <natansab@gmail.com>
2017-04-07 14:34:57 +0200

    ok working

---
+++ d04/ex01/create.php
@@ -0,0 +1,31 @@
+<?php
+if ($_POST["submit"] !== "OK" && $_POST["login"] !== "" && $_POST["passwd"] !== "") {
+	echo "ERROR\n";
+	return ;
+}
+$info_arr = array(
+	"login"		=> $_POST['login'],
+	"passwd"	=> hash("whirlpool", $_POST['passwd']),
+);
+if (!file_exists("./private"))
+	mkdir("./private", 0777, true);
+if (!file_exists("./private/passwd")) {
+	$main_arr = array($info_arr);
+	file_put_contents("./private/passwd", serialize($main_arr));
+	echo "OK\n";
+}
+else {
+	$main_arr = unserialize(file_get_contents("./private/passwd"));
+	foreach($main_arr as $elem)
+	{
+		if ($elem["login"] === $info_arr["login"])
+		{
+			echo "ERROR\n";
+			return ;
+		}
+	}
+	$main_arr[] = $info_arr;
+	file_put_contents("./private/passwd", serialize($main_arr));
+	echo "OK\n";
+}
+?>
