346d2ec05b5f84263f8d5bc07e6b7f970580ba04
Natansab <natansab@gmail.com>
2017-04-09 05:27:24 +0200

    ok

--- rush00/cart.php
+++ rush00/cart.php
@@ -3,7 +3,7 @@ session_start();
 include 'ft_tools.php';
 // echo "current cart: <br />";
 // var_dump($_SESSION['cart']);
-
+// echo "user:" . $_SESSION["loggued_on_user"];
 // Update Cart
 if ($_POST['less'] == '-')
 {
@@ -16,6 +16,8 @@ if ($_POST['more'] == '+')
 $_SESSION['cart'][intval($_POST['id'])] += 1;
 if ($_POST['remove'] == 'remove')
 $_SESSION['cart'][intval($_POST['id'])] = 0;
+
+// Buying process
 ?>
 <!DOCTYPE html>
 <html>
@@ -31,43 +33,50 @@ $_SESSION['cart'][intval($_POST['id'])] = 0;
 				<h2>Current Cart</h2>
 				<?php
 				if (isset($_SESSION['cart'])) {
-					$conn = mysqli_connect($servername, $username, $password, "SHOP_DATABASE");
-					if (!$conn) {
+					if (!$_SESSION['cart'] || !array_sum($_SESSION['cart']))
+					echo "Your cart is empty 👉 <a href=\"./index.php\">GO TO SHOP 🎉</a>";
+					else {
+						$conn = mysqli_connect($servername, $username, $password, "SHOP_DATABASE");
+						if (!$conn)
 						die("Connection failed: " . mysqli_connect_error());
-					}
-					foreach($_SESSION['cart'] as $id => $qty) {
-						$sql = "SELECT * FROM Products WHERE id = $id";
-						$result = mysqli_query($conn, $sql);
-						if (mysqli_num_rows($result) > 0 && $qty > 0) {
-							$row = mysqli_fetch_assoc($result);
-							?>
-							<div class='product'>
-								<img class='img_product' src=<?php echo $row["img_path"] ?> /><br />
-								Product Name: <?php echo $row["name"] ?> <br />
-								Quantity: <?php echo $qty ?> <br />
-								Price: $<?php echo $row["price"] ?> <br />
-								<form action='cart.php' method='post'>
-									<input type='hidden' name='id' value=<?php echo $id ?>>
-									<button name="less" value="-">-</button>
-									<button name="more" value="+">+</button>
-									<button name="remove" value="remove">Remove</button>
-								</form>
-								<br />
+						foreach($_SESSION['cart'] as $id => $qty) {
+							$sql = "SELECT * FROM Products WHERE id = $id";
+							$result = mysqli_query($conn, $sql);
+							if (mysqli_num_rows($result) > 0 && $qty > 0) {
+								$row = mysqli_fetch_assoc($result);
+								?>
+								<div class='product'>
+									<img class='img_product' src=<?php echo $row["img_path"] ?> /><br />
+									Product Name: <?php echo $row["name"] ?> <br />
+									Quantity: <?php echo $qty ?> <br />
+									Price: $<?php echo $row["price"] ?> <br />
+									<form action='cart.php' method='post'>
+										<input type='hidden' name='id' value=<?php echo $id ?>>
+										<button name="less" value="-">-</button>
+										<button name="more" value="+">+</button>
+										<button name="remove" value="remove">Remove</button>
+									</form>
+									<br />
+								</div>
+								<?php }} mysqli_close($conn); ?>
+								<div class="cart_total">
+									<hr />
+									<h3>Total : $<?php echo total_cart($_SESSION['cart']) ?></h3>
+									<form action='cart.php' method='post'>
+										<button name="submit" value="buy">Buy</button>
+									</form>
+									<?php
+									if ($_POST['submit'] == 'buy')
+										if (!isset($_SESSION["loggued_on_user"]) || $_SESSION["loggued_on_user"] == "")
+											echo "You must but loggued in to complete purchase 👉 <a href=\"login_index.php\">Log In</a>";
+									?>
+									<hr />
+								</div>
 							</div>
-							<?php }} mysqli_close($conn); }?>
-							<div class="cart_total">
-								<hr />
-								<h3>Total : $<?php echo total_cart($_SESSION['cart']) ?></h3>
-								<form>
-									<button name="submit" value="buy">Buy</button>
-								</form>
-								<hr />
+							<?php }}}?>
+							<div id="archieved_carts">
+								<h2>Past Orders</h2>
 							</div>
 						</div>
-						<?php }?>
-						<div id="archieved_carts">
-							<h2>Past Orders</h2>
-						</div>
-					</div>
-				</body>
-				</html>
+					</body>
+					</html>
--- rush00/create.php
+++ rush00/create.php
@@ -20,8 +20,6 @@ if (file_exists("./private/passwd")) {
 			return ;
 		}
 }
-$main_arr[] = $info_arr;
-file_put_contents("./private/passwd", serialize($main_arr));
 
 $conn = mysqli_connect($servername, $username, $password, "SHOP_DATABASE");
 $login = mysqli_real_escape_string($conn, $info_arr['login']);
@@ -31,6 +29,11 @@ $shipp_add = mysqli_real_escape_string($conn, $_POST['shipp_add']);
 $admin = ($_POST['admin'] == "42") ? 1 : 0;
 $sql = "INSERT INTO Customers (login, password, name, address, admin, status)
 VALUES ('$login', '$password', '$name', '$shipp_add', '$admin', 1);";
+
+$main_arr[] = $info_arr;
+file_put_contents("./private/passwd", serialize($main_arr));
+
+
 // if (mysqli_query($conn, $sql)) {
 //     echo "New record created successfully";
 // } else {
