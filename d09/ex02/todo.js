952128fb669cf9d1ec4843623c7cb0c6b0709f17
Natansab <natansab@gmail.com>
2017-04-15 13:58:25 +0200

    cookies ok

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,51 +1,63 @@
-var id_count = 5;
+id_count = (getCookie("elem_nb") == "") ? 0 : parseInt(getCookie("elem_nb"));
+
+// Duree du cookie
+var d = new Date();
+d.setTime(d.getTime() + (10*24*60*60*1000));
+var expires = "expires="+ d.toUTCString();
 
 function newElem() {
+
 	var text_elem = prompt("Quoi de prevu ajd ?");
-	var ftlist_elem = document.getElementById('ft_list');
 	if (text_elem == null)
 	 	return ;
 	var div_elem = document.createElement('div');
+	var ftlist_elem = document.getElementById('ft_list');
 	div_elem.innerHTML = "<span onclick='delElem(" + id_count + ")'>" + text_elem + "</span>";
 	div_elem.id = id_count;
+	createCookie(id_count, text_elem, expires);
+	createCookie("elem_nb", id_count, expires);
 	id_count++;
-
 	ftlist_elem.insertBefore(div_elem, ftlist_elem.children[3]);
-	console.log(text_elem);
 }
 
-// function delElem(id_elem) {
-// 	if (confirm("Sur de voitre choix ?")) {
-// 		var elem = document.getElementById(id_elem);
-// 		elem.parentNode.removeChild(elem);
-// 	}
-//
-// }
-//
-// function createCookie(name,value,days) {
-// 	if (days) {
-// 		var date = new Date();
-// 		date.setTime(date.getTime()+(days*24*60*60*1000));
-// 		var expires = "; expires="+date.toGMTString();
-// 	}
-// 	else var expires = "";
-// 	document.cookie = name+"="+value+expires+"; path=/";
-// }
-//
-//
-// function readCookie(name) {
-// 	var nameEQ = name + "=";
-// 	var ca = document.cookie.split(';');
-// 	for(var i=0;i < ca.length;i++) {
-// 		var c = ca[i];
-// 		while (c.charAt(0)==' ')
-// 			c = c.substring(1,c.length);
-// 		if (c.indexOf(nameEQ) == 0)
-// 			return c.substring(nameEQ.length,c.length);
-// 	}
-// 	return null;
-// }
-//
-//
-// createCookie("elem1","shopping",100);
-// console.log(readCookie("elem1"));
+function delElem(id_elem) {
+	if (confirm("Sur de voitre choix ?")) {
+		var elem = document.getElementById(id_elem);
+		elem.parentNode.removeChild(elem);
+		createCookie(id_elem, "", 0);
+	}
+}
+
+function createCookie(name,value,expires) {
+	document.cookie = name + "=" + value + ";" + expires;
+}
+
+window.addEventListener('load', function() {
+	for(i = 0; i <= id_count; i++) {
+
+		var text_elem = getCookie(i);
+		if (text_elem != "") {
+		console.log(i + "  " + text_elem);
+		var div_elem = document.createElement('div');
+		var ftlist_elem = document.getElementById('ft_list');
+		div_elem.innerHTML = "<span onclick='delElem(" + i + ")'>" + text_elem + "</span>";
+		div_elem.id = i;
+		ftlist_elem.insertBefore(div_elem, ftlist_elem.children[3]);
+		}
+	}
+});
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
