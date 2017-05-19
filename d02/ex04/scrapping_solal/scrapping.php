1376c115d3783b1f110560aa5c2b0d9f0d376254
Natan Sabbah <nsabbah@e1z2r3p10.42.us.org>
2017-05-19 08:24:48 -0700

    new scrap

--- d02/ex04/scrapping_solal/scrapping.php
+++ d02/ex04/scrapping_solal/scrapping.php
@@ -12,7 +12,7 @@ $links = file($file_path);
 // 	return ;
 // }
 $fp = fopen("./data.csv", "w");
-fputcsv($fp, array("num_page","url", "nom_vin", "annee", "nb_etoiles", "prix", "coup de coeur"));
+fputcsv($fp, array("num_page","url", "nom_vin", "annee", "nb_etoiles", "prix", "coup de coeur", "garde", "production", "elevage", "temperature", "cepages", "address"));
 // foreach($links as $fields)
 // 	fputcsv($fp, array($fields));
 // fclose($fp);
@@ -31,7 +31,7 @@ curl_setopt($ch, CURLOPT_COOKIEJAR,       $cookie);
 curl_setopt($ch, CURLOPT_COOKIEFILE,      $cookie);
 
 curl_setopt ($ch, CURLOPT_POST, 1);
-curl_setopt ($ch,CURLOPT_POSTFIELDS,"_username=xxx&_password=xxx&_csrf_token=c55623574318681da51e350df25bad462a34ebab");
+curl_setopt ($ch,CURLOPT_POSTFIELDS,"_username=solal.sabbah@gmail.com&_password=galousab132&_csrf_token=683ef1b1a72b089af22b44a6e1392fd6f16f3d6f");
 // $str = curl_exec($ch);
 
 for($page_nb = 1; $page_nb <= 67; $page_nb++) {
@@ -44,11 +44,29 @@ for($page_nb = 1; $page_nb <= 67; $page_nb++) {
 	for ($i = 0; $i <= 14; $i++) {
 		$nb_etoiles = preg_match_all('/(red)/',$matches[4][$i], $nw);
 		$url = $matches[1][$i];
+		// var_dump($url);
 		curl_setopt($ch, CURLOPT_URL, $url);
 		$str = curl_exec($ch);
 		$fav = (preg_match_all('/(icon icon-heart-red)/', $str, $nw) == 1) ? "oui" : "non";
+		// var_dump($str);
+		// var_dump($nw);
+		preg_match_all('/GARDE :.*?value\">(.*?)<\/div>/s', $str, $garde);
+		preg_match_all('/PRODUCTION :.*?value\">(.*?)<\/div>/s', $str, $prod);
+		preg_match_all('/ÉLEVAGE :.*?value\">(.*?)<\/div>/s', $str, $elevage);
+		preg_match_all('/TEMPÉRATURE :.*?value\">(.*?)<\/div>/s', $str, $temp);
+		preg_match_all('/PRINCIPAUX CÉPAGES<\/div>(.*?)<\/td>/s', $str, $cepages);
+		preg_match_all('/<address>(.*?)<\/address>/s', $str, $address);
+		// var_dump($garde[1][0]);
+		// var_dump($prod[1][0]);
+		// var_dump($elevage[1][0]);
+		// var_dump($temp[1][0]);
+		// var_dump($cepages[1][0]);
+		// var_dump($address[1][0]);
+		// return ;
+		// var_dump($retour_preg);
 		echo $fav . "\n";
-		fputcsv($fp, array($page_nb, $matches[1][$i], $matches[2][$i], $matches[3][$i], $nb_etoiles, $matches[5][$i], $fav));
+		fputcsv($fp, array($page_nb, $matches[1][$i], $matches[2][$i], $matches[3][$i], $nb_etoiles, $matches[5][$i], $fav,
+												$garde[1][0], $prod[1][0], $elevage[1][0], $temp[1][0], $cepages[1][0], $address[1][0]));
 	}
 }
 
