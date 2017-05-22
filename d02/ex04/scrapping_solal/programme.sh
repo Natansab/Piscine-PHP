95c441d21f603c54719c2c7ca69a4ca2ab5ec4aa
Natan Sabbah <nsabbah@e1z2r3p10.42.us.org>
2017-05-22 14:25:05 -0700

    cleaned

--- d02/ex04/scrapping_solal/programme.sh
+++
@@ -1,5 +0,0 @@
-while read line
-do
-curl -g "$line" | grep '<div class="title"><a href="http://www.hachette-vins.com/guide-vins/les-vins/' >> pages
-done < links
-cut -f4 -d"\"" pages > information 
